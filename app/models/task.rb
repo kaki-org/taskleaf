cf15392a741e5fcb10e1072ec30b7663c68ddbe6
kakikubo teruo <kakikubo@gmail.com>
2019-06-09 20:14:24 +0900

    implement test for nameless validate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  #before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
@@ -8,4 +9,8 @@ class Task < ApplicationRecord
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含める事はできません') if name&.include?(',')
   end
+
+  #def set_nameless_name
+  #  self.name = '名前なし' if name.blank?
+  #end
 end
