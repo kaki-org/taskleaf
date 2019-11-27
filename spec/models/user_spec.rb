87348aead17ba546ac7b4f4b13eb3015ece7eab9
kakikubo teruo <kakikubo@gmail.com>
2019-11-27 22:54:57 +0900

    add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,50 @@
+# frozen_string_literal: true
+
 # require 'rspec'
 require 'rails_helper'
 
 describe 'User' do
   # 名とメールとパスワードがあれば有効な状態であること
-  it "is valid with a name and email and password" do
+  it 'is valid with a name and email and password' do
     user = User.new(
-                   name: 'kakikubo',
-                   email: 'kakikubo@gmail.com',
-                   password: 'password',
+      name: 'kakikubo',
+      email: 'kakikubo@gmail.com',
+      password: 'password'
     )
     expect(user).to be_valid
   end
   # 名がなければ無効な状態であること
-  it "is invalid without a name"
+  it 'is invalid without a name' do
+    user = User.new(
+      name: nil
+    )
+    user.valid?
+    expect(user.errors[:name]).to include('を入力してください')
+  end
   # メールアドレスがなければ無効な状態であること
-  it "is invalid without a email"
+  it 'is invalid without a email' do
+    user = User.new(
+      name: 'kakikubo',
+      password: 'password'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('を入力してください')
+  end
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      name: 'kakikubo',
+      password: 'password',
+      email: 'kakikubo@gmail.com'
+    )
+    user = User.new(
+      name: 'teruo',
+      password: 'password',
+      email: 'kakikubo@gmail.com'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('はすでに存在します')
+  end
   # 連絡先のフルネームを文字列として返すこと
   it "return a users's name as a string"
   # before do
