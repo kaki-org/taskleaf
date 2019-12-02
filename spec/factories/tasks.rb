457e0ca95b61502129e1b9358205b37d4c154cf7
kakikubo teruo <kakikubo@gmail.com>
2019-12-02 22:44:57 +0900

    add task_spec.rb

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,8 +2,17 @@
 
 FactoryBot.define do
   factory :task do
+    association :user
     name { 'テストを書く' }
-    description { 'RSpec&Capybara&FactoryBotを準備する' }
-    user
+
+    factory :default_task do
+      description { 'RSpec&Capybara&FactoryBotを準備する' }
+    end
+    factory :mass_task do
+      description { '数学のFactoryBotを準備する' }
+    end
+    factory :society_task do
+      description { '社会科のFactoryBotを準備する'}
+    end
   end
 end
