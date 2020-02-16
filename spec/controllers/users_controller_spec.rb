5f289a07d05b40ed0245d39efb25938f82c90c02
kakikubo teruo <kakikubo@gmail.com>
2020-02-16 23:04:42 +0900

    shared_exampleを使ってコードを簡略化(ただし、管理者以外の一般、ゲストはうまくいってない)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,12 +3,7 @@
 require 'rails_helper'
 
 describe Admin::UsersController do
-  describe 'administrator access' do
-    before :each do
-      user = create(:admin)
-      session[:user_id] = user.id
-    end
-
+  shared_examples 'public access to users' do
     describe 'GET #index' do
       let(:users) { FactoryBot.create_list :user, 2 }
       # params[:limitがある場合]
@@ -39,6 +34,8 @@ describe Admin::UsersController do
       end
     end
 
+  end
+  shared_examples 'full access to users' do
     describe 'GET #show' do
       # @user に要求された連絡先を割り当てる事
       it 'assigns the requested user to @user' do
@@ -53,7 +50,6 @@ describe Admin::UsersController do
         expect(response).to render_template :show
       end
     end
-
     describe 'PATCH #update' do
       before :each do
         @user = create(:user, name: 'kakikubo', email: 'kakikubo@example.com', password: 'password')
@@ -111,24 +107,23 @@ describe Admin::UsersController do
       end
     end
   end
+  describe 'administrator access' do
+    before :each do
+      user = create(:admin)
+      session[:user_id] = user.id
+    end
+    it_behaves_like 'public access to users'
+    it_behaves_like 'full access to users'
+  end
   describe '一般ユーザでのアクセス' do
     before :each do
       user = create(:user)
       session[:user_id] = user.id
     end
-
-    # スペックは管理者用と同じものを用意するつもりだった
-    describe 'GET #index' do
-      let(:users) { FactoryBot.create_list :user, 2 }
-      context 'ユーザ一覧画面へ遷移しようとする' do
-        it 'ルートURLへ飛ばされる' do
-          get :index
-          expect(response).to redirect_to root_url
-        end
-      end
-    end
+    it_behaves_like 'public access to users'
   end
-  describe 'ゲストアクセスで' do
+  describe 'ゲストユーザでのアクセス' do
+    it_behaves_like 'public access to users'
     describe 'GET #new 新規作成しようとして' do
       it 'ログインを要求すること' do
         get :new
