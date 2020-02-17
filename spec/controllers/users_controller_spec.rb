3b15e2f69c7a4f23b67f25f9d6dbbb119a099eb4
kakikubo teruo <kakikubo@gmail.com>
2020-02-18 00:51:18 +0900

    マクロの定義と利用する為のrails_helper.rbでの設定変更

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -153,14 +153,14 @@ describe Admin::UsersController do
   describe 'administrator access' do
     before :each do
       user = create(:admin)
-      session[:user_id] = user.id
+      set_user_session(user)
     end
     it_behaves_like 'full access to users'
   end
   describe '一般ユーザでのアクセス' do
     before :each do
       user = create(:user)
-      session[:user_id] = user.id
+      set_user_session(user)
     end
     it_behaves_like 'public access to users'
   end
