22915951f90896517902b97c248c9e8499e927c2
kakikubo teruo <kakikubo@gmail.com>
2020-02-23 23:33:00 +0900

    CSVの検証をしてみる

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,14 +2,16 @@
 
 require 'rails_helper'
 
-# FIXME: このファイル自体が未完。ログイン処理が実装されたらp.70に戻って実装する
 describe TasksController do
   describe 'CSV出力' do
+    before :each do
+      save_user_session create(:user)
+    end
     it 'CSVファイルを返すこと' do
       get :index, params: { format: :csv }
       expect(response.headers['Content-Type']).to match 'text/csv'
     end
-    xit '中身を返すこと' do
+    it '中身を返すこと' do
       create(:task, name: 'title')
     end
   end
