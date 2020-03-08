42793f1067512e8a552e31402845c8af6e67d009
kakikubo teruo <kakikubo@gmail.com>
2020-03-08 19:19:40 +0900

    APIのテストから先につくりはじめる

---
+++ spec/requests/api/v1/tasks_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'タスクAPI', type: :request do
+  let!(:task) { FactoryBot.create(:task, name: 'メイラーSpecを書く', description: '送信したメールの内容を確認します') }
+  context '作成したタスク' do
+    it '作成したタスクが返却される事' do
+      get "/api/v1/tasks/#{task.id}", nil, { 'HTTP_ACCEPT' => 'application/vnd.tasks.v1' }
+
+      expect(response).to have_http_status(:success)
+
+      json = JSON.parse(response.body)
+      expect(json['name']).to eq task.name
+      expect(json['description']).to eq task.description
+    end
+  end
+end
