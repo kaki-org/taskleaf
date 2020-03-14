276f56b3ed6a59e614a90c14f40cf65463e04ba6
kakikubo teruo <kakikubo@gmail.com>
2020-03-15 00:19:27 +0900

    nested

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-class Api::V1::TasksController < ApplicationController
-  skip_before_action :login_required
-  def show
-    render json: Task.find(params[:id])
+class Api < ApplicationController
+  class V1
+    class TasksController
+      skip_before_action :login_required
+      def show
+        render json: Task.find(params[:id])
+      end
+    end
   end
 end
