5d2ad5edaaa778362505e5b7b638871fbcb273c6
kakikubo teruo <kakikubo@gmail.com>
2020-03-15 00:22:50 +0900

    わからんくなってきた

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-class Api < ApplicationController
-  class V1
-    class TasksController
-      skip_before_action :login_required
-      def show
-        render json: Task.find(params[:id])
-      end
-    end
+class Api::V1::TasksController < ApplicationController
+  skip_before_action :login_required
+
+  def show
+    render json: Task.find(params[:id])
   end
 end
