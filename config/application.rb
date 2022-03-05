6427e681493ce43aad235315cec57ab8621fa95c
kakikubo <kakikubo@gmail.com>
2022-03-05 18:44:58 +0900

    fix default

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,7 +12,7 @@ Bundler.require(*Rails.groups)
 module Taskleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
