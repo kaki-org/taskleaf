4698d5a73256c587bea955d90c07e982f0bee09e
kakikubo <kakikubo@gmail.com>
2023-01-19 05:31:15 +0900

    よくわからないけどコミットしてしまう

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -108,7 +108,7 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system, js: true) do
     driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    # Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
     Capybara.server_port = 3000
     Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
