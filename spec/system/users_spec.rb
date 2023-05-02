ed24d61f5920f666864104b95c8289d9d6bf91da
kakikubo <kakikubo@gmail.com>
2023-05-03 08:26:10 +0900

    うまく遷移できていない模様なのでリロードして画面を再描画する

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -135,7 +135,7 @@ describe 'タスク管理機能', type: :system do
     end
     context '削除ボタンを押す' do
       it '確認ダイアログが表示される' do
-        visit task_path(@task) # うまく遷移できてないことが多いのでリロードの意味で再度
+        visit task_path(@task) # うまく遷移できてないことが多いのでリロードの意味で再度visitを呼び出す
         click_link '削除'
         expect(page.driver.browser.switch_to.alert.text).to eq 'タスク「次のタスク」を削除します。よろしいですか？'
         # expect(page.driver.browser.accept_js_confirms.text).to eq 'タスク「次のタスク」を削除します。よろしいですか？'
