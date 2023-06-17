9cb401c4569e99bba564c8b835f948e400d1f85b
kakikubo <kakikubo@gmail.com>
2023-06-17 21:56:56 +0900

    fix by rubocop

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  describe "create contact" do
-    it "should create contact" do
+  describe 'create contact' do
+    it 'should create contact' do
       contact = Contact.new
-      contact.name = "test"
-      contact.email = "test@example.com"
+      contact.name = 'test'
+      contact.email = 'test@example.com'
       contact.save
       expect(contact).to be_valid
     end
