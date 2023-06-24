d945a0d6263bec59e7f77474e581e6b33725aadd
kakikubo <kakikubo@gmail.com>
2023-06-24 18:02:05 +0900

    不要なルーティングとコメントを削除

--- config/routes.rb
+++ config/routes.rb
@@ -11,20 +11,9 @@ Rails.application.routes.draw do
   end
   namespace :admin do
     resources :users
-    # get 'users/new'
-    # get 'users/edit'
-    # get 'users/show'
-    # get 'users/index'
   end
   root to: 'tasks#index'
   resources :tasks do
-    post :confirm, action: :confirm_new, on: :new
     post :import, on: :collection
   end
-  # similiar to
-  # get 'tasks/index'
-  # get 'tasks/show'
-  # get 'tasks/new'
-  # get 'tasks/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
