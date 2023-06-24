0c77b27f822eee7e02c4777c6f83b9acee0833bc
kakikubo <kakikubo@gmail.com>
2023-06-24 13:10:04 +0900

    legacy.rbのテストコードを追加

--- spec/lib/legacy_spec.rb
+++ spec/lib/legacy_spec.rb
@@ -4,8 +4,14 @@ require 'rails_helper'
 require 'legacy'
 
 describe Legacy do
+  let!(:users) do
+    10.times do
+      FactoryBot.create(:user)
+    end
+  end
   it 'userからcontactを作成すること' do
     Legacy.move_contact
     expect(User.count).to eq Contact.count
+    expect(Contact.count).to eq 10 # 10.timesで作成したので
   end
 end
