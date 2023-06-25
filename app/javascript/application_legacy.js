7ef190ff27b327cc28daef76763c783f4bcbf2cb
kakikubo <kakikubo@gmail.com>
2023-06-25 16:32:42 +0900

    ログアウトできない問題を解消(回避)

--- app/javascript/application_legacy.js
+++ app/javascript/application_legacy.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ../../vendor/bundle/ruby/3.2.0/gems/activestorage-7.0.4.3/app/assets/javascripts/activestorage
+//= require ../../vendor/bundle/ruby/3.2.0/gems/activestorage-7.0.5/app/assets/javascripts/activestorage
 //= require_tree .
