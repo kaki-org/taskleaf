8d79d524a48eb647b4ef15b5de80cf5e52d93d9f
kakikubo <kakikubo@gmail.com>
2023-11-23 15:49:14 +0900

    render-build.shを更新

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,13 @@
 set -o errexit
 
 #env
+curl -fsSL https://bun.sh/install | bash
+export BUN_INSTALL="$HOME/.bun"
+export PATH=$BUN_INSTALL/bin:$PATH
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle install
+
 echo "db:migrate"
 bundle exec rails db:migrate
 bundle exec rails db:seed
